5a544357e12c76c2ba289a7a0d9705ef87733eb3
Imran Khan <imrankhan@Imrans-MacBook-Air.local>
2022-03-05 18:18:37 +0530

    twst

--- app/controllers/api/index.js
+++ app/controllers/api/index.js
@@ -6,5 +6,10 @@ router.use('/v1', v1Router);
 console.log("hello")
 console.log("hello")
 console.log("hello")
-
+let i = 1
+if(true){
+  if(i == 1){
+    console.log("hello")
+  }
+}
 module.exports = router;
